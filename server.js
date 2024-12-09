a75c2f51be167ee3e3d3c2e9805cb3c4e19f8c14
ivan.dimitrov <ivan.dimitrov@fit.fraunhofer.de>
2024-12-09 23:34:24 +0100

    all cors

--- server.js
+++ server.js
@@ -4,14 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors({
-    origin: [
-      'http://localhost:3000',
-      'https://air.thedrop.top:4000',
-      '*'
-    ],
-    credentials: true
-}));
+app.use(cors());
 
 let cachedData = null;
 let lastFetchTime = null;
